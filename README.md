b88d0a157c229f6aa8a8ecee574b687b09554fc1
AmasterJT <amasterjt@gmail.com>
2024-09-05 00:22:14 +0200

    mejoramos la documentacion

---
+++ README.md
@@ -0,0 +1,21 @@
+# Separador de Archivos
+
+Este script en Python organiza archivos en una carpeta especificada en dos subcarpetas: "FOTOS" y "VIDEOS". Dependiendo de la extensión del archivo, se moverá a la subcarpeta correspondiente.
+
+## Características
+
+- **Separación Automática**: Los archivos se organizan en "FOTOS" o "VIDEOS" basándose en su extensión.
+- **Soporte para Extensiones**: Utiliza una lista predefinida de extensiones para identificar imágenes y videos.
+- **Modo Verbose**: Imprime mensajes detallados sobre los archivos que se mueven, si se habilita el modo verbose.
+
+## Requisitos
+
+- Python 3.x
+- Paquetes Python:
+  - `rich` (para impresión con formato y colores)
+
+Para instalar los paquetes requeridos, usa el siguiente comando:
+
+```bash
+pip install rich
+```
--- SeparadorImganesVideos.py
+++ SeparadorImganesVideos.py
@@ -1,16 +1,25 @@
 import os
-import shutil
-import argparse
+import shutil  # Para mover archivos y directorios
+import argparse  # Para manejar argumentos de línea de comandos
+from rich import print as rprint  # Para imprimir mensajes con formato y colores
 
-from rich import print as rprint
-
+# Importa las extensiones de archivos desde un módulo externo
 from extensiones import EXTENSIONES
 
 
-def separar_archivos(carpeta_original, verbose: False):
+def separar_archivos(carpeta_original, verbose=False):
+    """
+    Separa archivos en la carpeta original en dos subcarpetas: "FOTOS" y "VIDEOS".
+    Los archivos se mueven a la carpeta correspondiente según su extensión. 
 
-    carpeta_fotos = carpeta_original + "/FOTOS"
-    carpeta_videos = carpeta_original + "/VIDEOS"
+    Parámetros:
+    carpeta_original (str): Ruta a la carpeta que contiene los archivos a separar.
+    verbose (bool): Si es True, imprime mensajes detallados sobre los archivos movidos.
+    """
+    # Definir rutas para las carpetas de destino
+    carpeta_fotos = os.path.join(carpeta_original, "FOTOS")
+    carpeta_videos = os.path.join(carpeta_original, "VIDEOS")
+
     # Crear las carpetas "FOTOS" y "VIDEOS" si no existen
     for carpeta in [carpeta_fotos, carpeta_videos]:
         if not os.path.exists(carpeta):
@@ -19,20 +28,19 @@ def separar_archivos(carpeta_original, verbose: False):
     # Obtener la lista de archivos en la carpeta original
     archivos = os.listdir(carpeta_original)
 
-    # Extensiones comunes de imágenes
+    # Copiar las extensiones de imágenes y videos desde el módulo EXTENSIONES
     extensiones_imagenes = EXTENSIONES.IMAGENES.copy()
-    # Extensiones comunes de videos
     extensiones_videos = EXTENSIONES.VIDEOS.copy()
 
     # Recorrer cada archivo en la carpeta original
     for archivo in archivos:
-        # Obtener la extensión del archivo
+        # Obtener la extensión del archivo (en minúsculas para comparación)
         _, extension = os.path.splitext(archivo.lower())
 
         # Construir la ruta completa del archivo
         ruta_archivo = os.path.join(carpeta_original, archivo)
 
-        # Determinar si es una imagen o un video y moverlo a la carpeta correspondiente
+        # Determinar si el archivo es una imagen o un video y moverlo a la carpeta correspondiente
         if extension in extensiones_imagenes:
             shutil.move(ruta_archivo, os.path.join(carpeta_fotos, archivo))
             if verbose:
@@ -41,40 +49,36 @@ def separar_archivos(carpeta_original, verbose: False):
             shutil.move(ruta_archivo, os.path.join(carpeta_videos, archivo))
             if verbose:
                 print(f"Se movió '{archivo}' a la carpeta 'VIDEOS'")
-        elif os.path.isdir(carpeta_original + f"\{archivo}"):
-            rprint("[yellow]" + f"'{archivo}' es un diretorio" + "[/yellow]")
+        elif os.path.isdir(ruta_archivo):
+            rprint("[yellow]" + f"'{archivo}' es un directorio" + "[/yellow]")
         else:
-            rprint("[red]" +
-                   f"El archivo '{archivo}' no es una imagen ni un video y no se movió."+"[/red]")
+            rprint(
+                "[red]" + f"El archivo '{archivo}' no es una imagen ni un video y no se movió." + "[/red]")
 
 
 def main():
-
-    # Crear el objeto ArgumentParser
+    """
+    Función principal que configura el análisis de argumentos de línea de comandos
+    y llama a la función separar_archivos con los argumentos proporcionados.
+    """
+    # Crear el objeto ArgumentParser para manejar los argumentos de línea de comandos
     parser = argparse.ArgumentParser(
-        description='Descripción de lo que hace el script.')
+        description='Organiza archivos en carpetas "FOTOS" y "VIDEOS" basándose en su extensión.')
 
-    # Agregar parametros
+    # Agregar parámetro para la opción verbose
     parser.add_argument('--verbose', '-v', action='store_true',
-                        help='Imprimir mensajes detallados.')
-    # Agregar argumentos
-    parser.add_argument('input', type=str,
-                        help='Ruta del archivo de la carpeta.')
+                        help='Imprimir mensajes detallados sobre el proceso.')
 
-    # Parsear los argumentos
+    # Agregar argumento posicional para la ruta de la carpeta de entrada
+    parser.add_argument('input', type=str,
+                        help='Ruta a la carpeta que contiene los archivos a organizar.')
+
+    # Parsear los argumentos de la línea de comandos
     args = parser.parse_args()
 
-    separar_archivos(
-        args.input, args.verbose)
+    # Llamar a la función separar_archivos con los argumentos proporcionados
+    separar_archivos(args.input, args.verbose)
 
 
 if __name__ == "__main__":
-    # Especifica la ruta de la carpeta original y las carpetas de destino
-    # ruta_carpeta_original = "D:\Imágenes\imágenes-iphone8-2024"
-    # ruta_carpeta_fotos = "ruta/a/tu/carpeta/FOTOS"
-    # ruta_carpeta_videos = "ruta/a/tu/carpeta/VIDEOS"
-
-    # Llama a la función para separar los archivos
-    # separar_archivos(ruta_carpeta_original)
-
     main()
